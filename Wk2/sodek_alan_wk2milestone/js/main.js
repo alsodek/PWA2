3409bbb94b6c655d1fccb65a0bc3fb4d394757f9
alsodek <alansodek@alans-mbp.att.net>
2014-06-11 22:10:12 -0500

    completed accordion box

--- Wk2/sodek_alan_wk2milestone/js/main.js
+++ Wk2/sodek_alan_wk2milestone/js/main.js
@@ -62,6 +62,48 @@
         .css({ top: mousey, left: mousex })
     });
 		
+    
+     /*
+	===============================================
+	======================================== Tabbed Accordion	
+	*/
+    
+    $('ul.tabs').each(function(){
+        // Keep track of each tab and which one is active.
+        var $active, $content, $links = $(this).find('a');
+        
+        // If location.hash matches a link use that tab.
+        // If no match is found, use first link.
+        $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+        $active.addClass('active');
+        
+        $content = $($active[0].hash);
+        
+        // Hide the remaining content.
+        $links.not($active).each(function () {
+            $(this.hash).hide();
+        });
+        
+        // Bind the click event handler
+        $(this).on('click', 'a', function(e){
+            // Make old tab inactive
+            $active.removeClass('active');
+            $content.hide();
+            
+            // Update the variables with the new link and content.
+            $active = $(this);
+            $content = $(this.hash);
+            
+            // Make the tab active.
+            $active.addClass('active');
+            $content.show();
+            
+            // Prevent the default
+            e.preventDefault();
+        });
+    });
+    
+    
 	/*
 	===============================================
 	======================================== Add Modal	
